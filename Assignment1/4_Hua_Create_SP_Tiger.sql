9bd0dbdc76993683afb30aa07d348a390fa54992
wangh21 <wangh21@student.op.ac.nz>
2019-09-17 14:59:25 +1200

    update

--- Assignment1/4_Hua_Create_SP_Tiger.sql
+++ Assignment1/4_Hua_Create_SP_Tiger.sql
@@ -4,6 +4,8 @@ GO
 DROP PROC createCustomer
 DROP PROC createQuote
 DROP PROC addQuoteComponent
+DROP PROC updateAssemblyPrices
+DROP PROC testCyclicAssembly
 GO
 
 CREATE OR ALTER PROC createCustomer(@name NVARCHAR(100),
@@ -81,38 +83,111 @@ END
 GO
 
 
+CREATE TRIGGER trig_cu_AssemblySubcomponent ON AssemblySubcomponent
+AFTER UPDATE
+AS
+BEGIN
+	select * from Contact
+END
+GO
+
+
+CREATE TRIGGER trigSupplierDelete ON Supplier
+FOR DELETE
+AS
+BEGIN
+	
+	DECLARE @XYZ INT
+	DECLARE @K INT
+	SET @XYZ = (SELECT SupplierID FROM inserted)
+	SET @K = (SELECT COUNT(*) FROM Component WHERE SupplierID=@XYZ)
+	IF @K >= 1
+	PRINT(@XYZ + ' HAS ' + @K + ' related components.')
+END
+GO
+
+
 CREATE OR ALTER PROC updateAssemblyPrices
 AS
 BEGIN
-    DECLARE @i INT = 0;
+    DECLARE @ATP FLOAT(8)
+    DECLARE @ALP FLOAT(8)
 
-    DECLARE @myPrices TABLE
-	(
-        ATP FLOAT(8),
-        ALP FLOAT(8)
-	)
+    DECLARE cursor1 CURSOR FOR 
+	SELECT *
+    FROM
+        (SELECT SUM(c.TradePrice) AS 'ATP', SUM(c.ListPrice) AS 'ALP'
+        FROM Component c JOIN AssemblySubcomponent a ON c.ComponentID=a.SubcomponentID
+        GROUP BY a.AssemblyID)
+	AS T1
 
-    INSERT INTO @myPrices
-    SELECT top 1
-        SUM(c.TradePrice) AS 'ATP', SUM(c.ListPrice) AS 'ALP'
-    FROM Component c
-        JOIN AssemblySubcomponent a ON c.ComponentID=a.SubcomponentID
-    GROUP BY a.AssemblyID;
+    OPEN cursor1
 
-    WHILE (@i<3)
+    FETCH NEXT FROM cursor1 INTO @ATP,@ALP
+
+    WHILE @@FETCH_STATUS=0
 	BEGIN
-        UPDATE Component
-		SET TradePrice = (SELECT ATP
-        FROM @myPrices),
-        ListPrice =	(SELECT ALP
-        FROM @myPrices)
-        WHERE ComponentID IN
-        (SELECT DISTINCT AssemblyID
+        print @ATP
+        PRINT @ALP
+        UPDATE Component 
+		SET TradePrice=@ATP, ListPrice=@ALP 
+		WHERE ComponentID IN
+        (SELECT AssemblyID
         FROM AssemblySubcomponent
-        GROUP BY AssemblyID);
-
-        SET @i = @i + 1
+        GROUP BY AssemblyID)
+        FETCH NEXT FROM cursor1 INTO @ATP,@ALP
     END
-
+    CLOSE cursor1
+    DEALLOCATE cursor1
 END
 GO
+
+--BEGIN
+--    DECLARE @i INT = 0;
+
+--	DECLARE @myPrices TABLE
+--	(
+--        ATP FLOAT(8),
+--        ALP FLOAT(8)
+--	)
+
+--    INSERT INTO @myPrices
+--    SELECT top 1 SUM(c.TradePrice) AS 'ATP', SUM(c.ListPrice) AS 'ALP'
+--    FROM Component c
+--        JOIN AssemblySubcomponent a ON c.ComponentID=a.SubcomponentID
+--    GROUP BY a.AssemblyID;
+
+--    WHILE (@i<3)
+--	BEGIN
+--        UPDATE Component
+--		SET TradePrice = (SELECT ATP
+--        FROM @myPrices),
+--        ListPrice =	(SELECT ALP
+--        FROM @myPrices)
+--        WHERE ComponentID IN
+--        (SELECT DISTINCT AssemblyID
+--        FROM AssemblySubcomponent
+--        GROUP BY AssemblyID);
+
+--        SET @i = @i + 1
+--    END
+
+--END
+
+
+CREATE OR ALTER PROC testCyclicAssembly(@assemblyID INT,
+    @isCyclic INT OUTPUT)
+AS
+BEGIN
+    IF (SELECT COUNT(AssemblyID)
+    FROM AssemblySubcomponent
+    WHERE AssemblyID=(SELECT SubcomponentID
+    FROM AssemblySubcomponent
+    where SubcomponentID=@assemblyID)) >= 1
+        SET @isCyclic = 1;
+    ELSE
+        SET @isCyclic = 0;
+END
+RETURN @isCyclic
+GO
+
